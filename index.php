7ebaf4f1acab033a32290525d090fb9cbbdc28a4
Alvyn Velazquez <alvyn1010@gmail.com>
2021-03-10 12:07:35 -0500

    Removed Latin because it was bothersome

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
                     }
             ?>
             <h1>Insert information here about main site(Slogan)</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean at ante ut arcu semper eleifend vitae ac magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Ut sit amet suscipit ligula. Fusce sit amet mollis massa, ac tristique enim. Aenean iaculis enim id odio sagittis scelerisque. Aenean congue, augue eget imperdiet egestas, sem est hendrerit elit, et blandit mi leo nec orci. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+            <p>Insert more Information here.</p>
 
         </div>
     </section>
