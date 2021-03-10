480666df1eaa12f10de7a20fedd2e35c0557f36a
Alvyn Velazquez <alvyn1010@gmail.com>
2021-03-10 08:37:34 -0500

    Refactored Header to seperate .php file to reference across all pages.

---
+++ php/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta name ="viewport" content="width = device-width, initial-scale=1">
+    <meta name="description" content="Protect Yourself and Others with Covid.io" />
+    <meta name="author" content="Alvyn Velazquez, Hanbo Tan, Alana, Scott...etc" />
+    <link href="https://fonts.googleapis.com/css2?family=Public+Sans:wght@300;800&display=swap" rel="stylesheet">
+    <title>Covid.io</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+
+</head>
+<body>
+    <header class ="header">
+        <nav>
+            <div class="wrapper">
+            <a href="/">
+                <img src ="img/logo1.png"class="logo" alt="Covid.io"/>
+            </a>
+            <ul>
+                <a href="..index.php"class="header__link1">Home</a></li>
+                <a href="html/about.html"class="header__link2">About</a></li>
+                <a href="php/login.php"class="header__link3">Login</a></li>
+                <a href="php/register.php"class="header__link3">Register</a></li>
+            </ul>
+        </nav>
+        <div class="wrapper">
+    </header>
