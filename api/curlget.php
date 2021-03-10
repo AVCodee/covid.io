659decc53279cb1764b225d2db56d12bbf67a26c
Alvyn Velazquez <alvyn1010@gmail.com>
2021-03-10 15:18:13 -0500

    Called and Displayed API Value from Covid Tracking Project

---
+++ api/curlget.php
@@ -0,0 +1,19 @@
+<?php
+    $ch = curl_init();
+    $api = "https://api.covidtracking.com/v1/us/current.json";
+    
+    curl_setopt($ch, CURLOPT_URL, $api);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    
+    $resp = curl_exec($ch);
+
+    if($e = curl_error($ch)){
+        echo $e;
+    }
+    else {
+        $decoded = json_decode($resp);
+    }
+
+    curl_close($ch);
+
+    
--- index.php
+++ index.php
@@ -1,13 +1,23 @@
 <?php
     include_once 'header.php';
+    include 'api/curlget.php';
 ?>
+
+
     <section id="mainsplash">
         <div class="container">
             <?php
             if (isset($_SESSION["useruid"])) {
                         echo "<p>Welcome " . $_SESSION["useruid"] . "</p>";
                     }
+                    
             ?>
+
+            <?php
+            echo "There are currently " . $decoded[0]->positive . " positive cases in the US.";
+            ?>
+            
+            
             <h1>Insert information here about main site(Slogan)</h1>
             <p>Insert more Information here.</p>
 
