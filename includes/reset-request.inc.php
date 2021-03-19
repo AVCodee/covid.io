abe3292ab3aebe4721cf0782cfdadef8e254a777
Alvyn Velazquez <alvyn1010@gmail.com>
2021-03-19 00:44:10 -0400

    Removed Unneccessary Files and Refactored

--- includes/reset-request.inc.php
+++ includes/reset-request.inc.php
@@ -9,7 +9,41 @@ if (isset($POST[reset-request-submit])){
 
     $expires = date("U") + 1800;
 
-    require 'dbh.inc.php'
+    require 'dbh.inc.php';
+
+    $userEmail = $_POST["email"];
+
+    $sql = "DELETE FROM pwdReset WHERE pwdResetEmail=?;";
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt, $sql)){
+        echo "There was an error!";
+        exit();
+    } else {
+        mysqli_stmt_bind_param($stmt, "s", $userEmail);
+        mysqli_stmt_execute($stmt);
+
+    }
+
+    $sql = "INSERT INFO pwdReset (pwdResetEmail, pwdResetSelector, pwdResetToken, pwdResetExpires) VALUES (?, ?, ?, ?);";
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt, $sql)){
+        echo "There was an error!";
+        exit();
+    } else {
+        $hashedToken = password_hash($token, PASSWORD_DEFAULT);
+        mysqli_stmt_bind_param($stmt, "ssss", $userEmail, $selector, $hashedToken, $expires);
+        mysqli_stmt_execute($stmt);
+    }
+
+    mysqli_stmt_close($stmt);
+    mysqli_close();
+
+    $to = $userEmail;
+    $subject = 'Reset Your Password for Covid.io';
+    $message = '<p>You are receiving this email because you requested a Password Reset on Covid.io. The link to reset your password is below. If you did not make this request, you can ignore this email</p>';
+    $message .= '<p>Here is your password reset link: </br>';
+    $message .= '<a href="' . $url . '">' . $url . '</a></p>';
+
 
 } else {
     header("Location: ../index.php");
