11516e37c3b82be9bfa046f21df02dfa4062b267
ALANA CRUMBLEY <acrum004@plattsburgh.edu>
2021-03-29 20:41:59 -0400

    added about section

--- about.php
+++ about.php
@@ -8,5 +8,10 @@
 </head>
 <body>
     <h2>A small group of software developers that want to help spread awareness of the current health crisis. Covid-19 Live updates and reports of cases.</h2>
+
+    <h3>About Covid-19 Pandemic: Vaccines in New York state are now available to residents aged 30 or older. 
+    Mask mandate is still in effect. Maximum capacity for outdoor gatherings for private residences is up to 25 people.
+
+     Symptoms to look out for: headache, fever, cough, muscle aches and more </h3>
 </body>
 </html>
