dd756cda59aeabf8fce50e9028fc95b74c99a0ee
Hanbo Tan <79545204+MathTeacher101@users.noreply.github.com>
2021-03-29 21:56:14 -0400

    Update about.php

--- about.php
+++ about.php
@@ -1,17 +1,36 @@
-﻿<?php
+<?php
     include_once 'header.php';
 ?>
 <html>
 <head>
     <meta charset="utf-8" />
     <title>About Covid.io</title>
+    <link rel="stylesheet" type="text/css" href="css/AboutStyle.css"/>
 </head>
 <body>
+    <div class="wrapper">
+    <h1>Covid.io About us page.</h1>
     <h2>A small group of software developers that want to help spread awareness of the current health crisis. Covid-19 Live updates and reports of cases.</h2>
-
-    <h3>About Covid-19 Pandemic: Vaccines in New York state are now available to residents aged 30 or older. 
-    Mask mandate is still in effect. Maximum capacity for outdoor gatherings for private residences is up to 25 people.
-
+    <p>The group of four from the SUNY Plattsburgh students are planing to create a website to help the community to learn and understand better for the current Covid-19 pandamic,
+    because there are so many people still been misleading the knowledge how to protect themselves and their families.<br> We decide to make a platform to share the correct informtions
+    and give the updating news for the users to looking for informations.</p>
+    </p>
+    
+    <h3>About Covid-19 Pandemic:</h3>
+    <p>Vaccines in New York state are now available to residents aged 30 or older.<br>
+     Mask mandate is still in effect. Maximum capacity for outdoor gatherings for private residences is up to 25 peopl </p>
+   
+    <h4>We want to know things from you</h3>
+    <p>Hopefully this is the correct way we can do something with the community It's the pleasure that the user are interesting to check our webiste we would happy to hear from your
+    what kind of the informations you are looking for about the Covid-19.</p>
+    
+    <h5>Our team members for this project</h4>
+    <p1>Developer: Hanbo Tan<br>
+        Website Frontend Designer: Alana Crumbley<br>
+        Website Backend Developer: Alvyn Velazquez<br>
+        Graphic Designer & Software Tester: Scott Price</p1>
      Symptoms to look out for: headache, fever, cough, muscle aches and more </h3>
+    </p>
+    </div>
 </body>
 </html>
