c8f2b220a7b73656bf417bef2c81c5ddef667946
Alvyn Velazquez <alvyn1010@gmail.com>
2021-03-30 14:37:27 -0400

    Implementing New Bootstrap CSS, New Background

--- about.php
+++ about.php
@@ -1,3 +1,6 @@
+﻿<!doctype html>
+<html lang="en">
+
 <?php
     include_once 'header.php';
 ?>
@@ -5,25 +8,25 @@
 <head>
     <meta charset="utf-8" />
     <title>About Covid.io</title>
-    <link rel="stylesheet" type="text/css" href="css/AboutStyle.css"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 </head>
 <body>
-    <div class="wrapper">
-    <h1>Covid.io About us page.</h1>
-    <h2>A small group of software developers that want to help spread awareness of the current health crisis. Covid-19 Live updates and reports of cases.</h2>
+    <div class="container">
+    <h1 class="display-4">Covid.io: <small class="text-muted">About Us</small></h1>
+    <h2 class="display-6">A small group of software developers that want to help spread awareness of the current health crisis. Covid-19 Live updates and reports of cases.</h2>
     <p>The group of four from the SUNY Plattsburgh students are planing to create a website to help the community to learn and understand better for the current Covid-19 pandamic,
     because there are so many people still been misleading the knowledge how to protect themselves and their families.<br> We decide to make a platform to share the correct informtions
     and give the updating news for the users to looking for informations.</p>
-    </p>
+    </p></br>
     
-    <h3>About Covid-19 Pandemic:</h3>
-    <p>Vaccines in New York state are now available to residents aged 30 or older.<br>
-     Mask mandate is still in effect. Maximum capacity for outdoor gatherings for private residences is up to 25 peopl
-    <br>Symptoms to look out for: headache, fever, cough, muscle aches and more </p>
+    <h3 class="display-6">About Covid-19 Pandemic:</h3>
+    <p>Vaccines in New York state are now available to residents aged 30 or older.</br>
+     Mask mandate is still in effect. Maximum capacity for outdoor gatherings for private residences is up to 25 people.
+</br><strong>Symptoms to look out for: headache, fever, cough, muscle aches and more </p></br>
    
     <h4>We want to know things from you</h3>
     <p>Hopefully this is the correct way we can do something with the community It's the pleasure that the user are interesting to check our webiste we would happy to hear from your
-    what kind of the informations you are looking for about the Covid-19.</p>
+    what kind of the informations you are looking for about the Covid-19.</p></br>
     
     <h5>Our team members for this project</h4>
     <p1>Developer: Hanbo Tan<br>
