fedf4b8d7372d38e0a676a3c78f81b1cac648ad1
Alvyn Velazquez <alvyn1010@gmail.com>
2021-03-30 16:00:33 -0400

    Initial commit on bootstrap

--- index.php
+++ index.php
@@ -17,8 +17,8 @@
                         echo "<p>Welcome " . $_SESSION["useruid"] . "</p>";
                     }
                     
-            ?>
-            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">    
+            ?> <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">    
+           
             <p>Welcome to the Covid.io.</br>
             <p>Providing all Covid.19 live updates and reports!
             If you feel any symptoms Covid Related Get tested today at a local clinic in your area to be sure.</br>
