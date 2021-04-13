3f23f04507c2d8a99bda53f974ae5bd26e43f610
Alvyn Velazquez <alvyn1010@gmail.com>
2021-04-13 16:14:14 -0400

    Updated Index.php with Response UI Fixes

--- index.php
+++ index.php
@@ -44,16 +44,18 @@
           <li class="nav-item">
             <a class="nav-link" href="about.html">About</a>
           </li>
+          <li>
           <?php
                     if (isset($_SESSION["useruid"])) {
                         echo "<a href='profile.php'class='nav-item'>My Profile</a></li>";
                         echo "<a href='includes/logout.inc.php'class='nav-item'>Log Out</a></li>";
                     }
                     else {
-                        echo "<a href='login.php'class='nav-item'>Login</a></li>";
-                        echo "<a href='register.php'class='nav-item'>Register</a></li>";
+                        echo "<li class='nav-item'> <a class='nav-link' href='login.php'>Login</a></li>";
+                        echo "<li class='nav-item'> <a class='nav-link' href='register.php'>Register</a></li>";
                     }
                     ?>
+                    
          
           <li class="nav-item">
             <a class="nav-link" href="info.html">Covid Info</a>
