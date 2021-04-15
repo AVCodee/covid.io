51d217bd80a9e07b3743fb0297a4c3a08f00b0b5
Alvyn Velazquez <alvyn1010@gmail.com>
2021-04-14 22:46:57 -0400

    Finalized Profile Page with New API Data

--- api/curlget.php
+++ api/curlget.php
@@ -12,10 +12,16 @@
     }
     else {
         $decoded = json_decode($resp);
-        echo "There are currently " . $decoded[0];
+        $x=0;
+        
+        
+        
+       
     }
+        
+        
+    
+        
+    
 
     curl_close($ch);
-
-    //https://www.vaccinespotter.org/api/v0/states/NY.json
-    
