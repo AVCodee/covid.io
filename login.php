7ef9281e7dd6ead3c570a2f0b2007b5740ff1daf
Hanbo Tan <79545204+MathTeacher101@users.noreply.github.com>
2021-04-29 01:10:31 -0400

    Update login.php

--- login.php
+++ login.php
@@ -3,84 +3,84 @@
 ?>
 <head>
 
+<!DOCTYPE html>
+<html lang="en">
 
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<meta name="description" content="">
-<meta name="author" content="">
 
-<title>Covid.io</title>
+<head>
 
-<!-- Bootstrap core CSS -->
-<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
 
-<!-- Custom fonts for this template -->
-<link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-<link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
-<link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+  <title>Covid.io-About Us</title>
 
-<!-- Custom styles for this template -->
-<link href="css/clean-blog.min.css" rel="stylesheet">
+
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+
+
+  <link href="css/clean-login.css" rel="stylesheet">
 
 </head>
 
 <body>
 
-<!-- Navigation -->
-<nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
-  <div class="container">
-    <a class="navbar-brand" href="index.html">Covid.io</a>
-    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-      Menu
-      <i class="fas fa-bars"></i>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarResponsive">
-      <ul class="navbar-nav ml-auto">
-        <li class="nav-item">
-          <a class="nav-link" href="index.php">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="about.php">About</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="login.php">Log-In</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href ="info.php">Covid Info</a>
-        </li>  
-      </ul>
-    </div>
-  </div>
-</nav>
 
-<!-- Page Header -->
-<header class="masthead" style="background-image: url('img/shotimage.jpg')">
-  <div class="overlay"></div>
-  <div class="container">
-    <div class="row">
-      <div class="col-lg-8 col-md-10 mx-auto">
-        <div class="site-heading">
-          <h1>Covid.io</h1>
-          <span class="subheading">Stay Healthy, Stay Safe</span>
+  <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand" href="index.html">Covid.io</a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        Menu
+        <i class="fas fa-bars"></i>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="index.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="about.php">About</a>
+          </li>
+          <li class="nav-item">
+              <a class="nav-linke" href ="info.php">Covid Info</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+
+  <header class="masthead" style="background-image: url('img/VaccineManBoy.jpg')">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <div class="page-heading">
+            <h1>Login to Covid.io</h1>
+            <span class="subheading">Login to Your Account </span>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-</header>
-    <meta charset="utf-8" />
-    <title>Login</title>
-    <link rel="stylesheet" type="text/css" href="css/clean-login.css" />
-</head>
-<body>
-    <section class="registerbox">
+  </header>
+  
+
+  <body>
+    <section class="container">
         <h2>Covid.io Login</h2>
-        <div class="registerbox">
+        <div class="form-group">
         <form action="includes/login.inc.php" method="post">
-            <input type="text" name="uid" placeholder="Username/Email" />
-            <input type="password" name="pwd" placeholder="Password" />
+            <input type="text" name="uid" placeholder="Username/Email" /><br>
+            <input type="password" name="pwd" placeholder="Password" /><br>
             <button type="submit" name="submit">Log In</button>
-            <a href="register.php">Sign up<br>
-            <a href="reset-password.php" button type="reset-password.php">Forgot password </button>
+            <button type="register.php">Sign up</button><br>
+            <button type="reset-password.php">Forgot password </button>
         </form>
         </div>
         
@@ -96,8 +96,24 @@
             
         }
         ?>
-    </section>
-</body>
+  <hr>
 
+
+  <footer>
+    <div class="container">
+          <p class="copyright text-muted">Covid.io 2021</p>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="js/clean-blog.min.js"></script>
+
+</body>
 
 </html>
