5cc6004a58b626a5922813fed16a2868879a9734
Hanbo Tan <79545204+MathTeacher101@users.noreply.github.com>
2021-05-03 23:06:28 -0400

    Update about.php

--- about.php
+++ about.php
@@ -66,7 +66,7 @@
       <div class="row">
         <div class="col-lg-8 col-md-10 mx-auto">
           <div class="page-heading">
-            <h1>About Me</h1>
+            <h1>About Us</h1>
             <span class="subheading">This is what we do.</span>
           </div>
         </div>
